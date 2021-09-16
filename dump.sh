9da35785cae3a546e1b35f34325db0fa85ee2775
Galaxy server privilege separation <galaxy@terraform-ga.garr.cloud.pa>
2021-09-16 16:06:43 +0000

    preserve permissions

--- dump.sh
+++ dump.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-sudo su - postgres -c 'pg_dump -f galaxy_tools.psql galaxy_tools'
+sudo su - postgres << BASH
+pg_dump -f galaxy_tools.psql galaxy_tools;
+BASH
 
-sudo su - root -c "
-mkdir -p /tmp/dump
-mv /var/lib/pgsql/galaxy_tools.psql /tmp/dump/dump.psql
-cp /home/galaxy/galaxy/config/shed_tool_conf.xml /tmp/dump
-tar cvzf /tmp/dump/tar_shed_tools.tar.gz /home/galaxy/galaxy/var/shed_tools/
-tar cvzf /tmp/dump/tar_conda.tar.gz /export/tool_deps/_conda"
+sudo su - root << ROOT
+mkdir -p /tmp/dump && chown -R galaxy:galaxy /tmp/dump ; 
+mv /var/lib/pgsql/galaxy_tools.psql /tmp/dump/dump.psql >/tmp/dump/dump.log &
+cp /home/galaxy/galaxy/config/shed_tool_conf.xml /tmp/dump >> /tmp/dump/dump.log &
+tar pcvzf /tmp/dump/tar_shed_tools.tar.gz /home/galaxy/galaxy/var/shed_tools/ >>/tmp/dump/dump.log && echo 'Shed_tool dump_package created' &
+tar pcvzf /tmp/dump/tar_conda.tar.gz /export/tool_deps/_conda >>/tmp/dump/dump.log && echo 'Conda dump_package created' & 
+wait
+echo "packages created"
+
+ROOT
